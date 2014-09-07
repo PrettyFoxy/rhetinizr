b9162da1f84d01e2aad069d8bfa0ccfe7e73663c
Alexandru Furculita <alex@rhetina.com>
2014-09-07 16:23:46 +0300

    No export

--- include/plugin/admincp.service_product_product_export.php
+++ include/plugin/admincp.service_product_product_export.php
@@ -3,4 +3,8 @@
  * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
  *
  * @see Admincp_Service_Product_Product::export()
- */ 
+ */
+
+if ($aRow['product_id'] === 'rhetinizr') {
+	$sDirectoryId = 'template';
+}
