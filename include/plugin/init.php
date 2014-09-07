dc9e3eb60a5eef9e6b777a1b6b54e31746ec3df3
Alexandru Furculita <alex@rhetina.com>
2014-09-07 11:03:36 +0300

    Enhanced init plugin

--- include/plugin/init.php
+++ include/plugin/init.php
@@ -4,13 +4,10 @@
  * Copyright (C) 2014 Alexandru Furculita <alex@rhetina.com>
  */
 
-include PHPFOX_DIR_MODULE . 'rhetinizr/headquarters/vendor/autoload.php';
-
-if (PHPFOX_DEBUG === true) {
-
-} else {
-
+if (false === file_exists( PHPFOX_DIR_MODULE . 'rhetinizr/headquarters/vendor/autoload.php' )) {
+	return;
 }
+include PHPFOX_DIR_MODULE . 'rhetinizr/headquarters/vendor/autoload.php';
 
 //function convert( $size )
 //{
@@ -19,8 +16,13 @@ if (PHPFOX_DEBUG === true) {
 //  return @round( $size / pow( 1024, ( $i = floor( log( $size, 1024 ) ) ) ), 2 ) . ' ' . $unit[$i];
 //}
 
-//$a = memory_get_usage( true )
-Rhetina::cacheClear();
-Rhetina::boot( 'dev', true );
+//$a = memory_get_usage( true );
+
+if (PHPFOX_DEBUG === true) {
+	Rhetina::cacheClear();
+	Rhetina::boot( 'dev', true );
+} else {
+	Rhetina::boot( 'prod', false );
+}
 //$b = memory_get_usage( true );
 //echo convert( $b - $a );
