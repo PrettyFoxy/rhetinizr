9e6f791ae717496779c4b462f107a53db5cef949
Alexandru Furculita <alex@rhetina.com>
2014-09-07 17:11:18 +0300

    xml replaced with yml

--- headquarters/libraries/Rhetina/Bundle/PhpfoxTamerBundle/DependencyInjection/PhpfoxTamerExtension.php
+++ headquarters/libraries/Rhetina/Bundle/PhpfoxTamerBundle/DependencyInjection/PhpfoxTamerExtension.php
@@ -3,20 +3,25 @@
 namespace Rhetina\Bundle\PhpfoxTamerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
+/**
+ * Class PhpfoxTamerExtension
+ *
+ * @package Rhetina\Bundle\PhpfoxTamerBundle\DependencyInjection
+ */
 class PhpfoxTamerExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-    }
+	public function load(array $configs, ContainerBuilder $container)
+	{
+		$loader = new YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+		$loader->load( 'services.yml' );
+	}
 
-    public function getAlias()
-    {
-        return 'phpfox_tamer';
-    }
+	public function getAlias()
+	{
+		return 'phpfox_tamer';
+	}
 }
