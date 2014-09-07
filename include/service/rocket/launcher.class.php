b643d24b3d9a57dbd94711e4d54d3dcf77117af7
Alexandru Furculita <alex@rhetina.com>
2014-09-07 15:44:06 +0300

    Fixed launcher paths

--- include/service/rocket/launcher.class.php
+++ include/service/rocket/launcher.class.php
@@ -5,16 +5,31 @@
  */
 class Rhetinizr_Service_Rocket_Launcher extends Phpfox_Service
 {
+	const MODULE = 'rhetinizr';
+	const INC = 'rhetina';
+
 	/**
 	 * Called when the module is installed
 	 */
 	public function on()
 	{
-		if (false === is_dir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/cache/' )) {
-			mkdir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/cache/' );
+		if (false === is_dir( PHPFOX_DIR_FILE . static::INC . DIRECTORY_SEPARATOR )) {
+			mkdir( PHPFOX_DIR_FILE . static::INC . DIRECTORY_SEPARATOR );
 		}
-		if (false === is_dir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/logs/' )) {
-			mkdir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/logs/' );
+		if (true === is_dir( PHPFOX_DIR_FILE . static::INC . DIRECTORY_SEPARATOR )) {
+			if (false === is_dir( PHPFOX_DIR_FILE . static::INC . '/cache/' )) {
+				mkdir( PHPFOX_DIR_FILE . static::INC . '/cache/' );
+			}
+			if (false === is_dir( PHPFOX_DIR_FILE . static::INC . '/logs/' )) {
+				mkdir( PHPFOX_DIR_FILE . static::INC . '/logs/' );
+			}
+		}
+
+		if (false === is_dir( PHPFOX_DIR_MODULE . static::MODULE . '/headquarters/' )) {
+			Phpfox::getLib( 'archive.extension.zip' )->extract(
+				PHPFOX_DIR_MODULE . static::MODULE . PHPFOX_DIR_MODULE_XML . '/packed-rocket-elements.zip',
+				PHPFOX_DIR_MODULE . static::MODULE
+			);
 		}
 	}
 
@@ -23,8 +38,35 @@ class Rhetinizr_Service_Rocket_Launcher extends Phpfox_Service
 	 */
 	public function off()
 	{
-		if (true === is_dir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/' )) {
-			rmdir( Phpfox::getParam( 'core.dir_file' ) . 'rhetina/' );
+		$this->deleteDirectory( PHPFOX_DIR_FILE . static::INC . DIRECTORY_SEPARATOR );
+	}
+
+	/**
+	 * @param $dir
+	 *
+	 * @return bool
+	 */
+	private function deleteDirectory($dir)
+	{
+		if (!file_exists( $dir )) {
+			return true;
 		}
+
+		if (!is_dir( $dir )) {
+			return unlink( $dir );
+		}
+
+		foreach (scandir( $dir ) as $item) {
+			if ($item == '.' || $item == '..') {
+				continue;
+			}
+
+			if (!$this->deleteDirectory( $dir . DIRECTORY_SEPARATOR . $item )) {
+				return false;
+			}
+
+		}
+
+		return rmdir( $dir );
 	}
 }
